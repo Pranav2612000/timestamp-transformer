29afb0562283da54bbba30e042c6b195810b6565
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-04-09 17:09:28 +0530

    feat: search for timestamps on the page

--- src/content.js
+++ src/content.js
@@ -1,5 +1,26 @@
+import { isTimestamp } from "./service/Timestamp";
+
 function loadContentScript() {
   console.log("Content script loaded");
+  const elements = Array.from(document.getElementsByTagName("*"));
+
+  elements.forEach((element) => {
+    const { childNodes } = element;
+
+    childNodes.forEach((node) => {
+      if (node.nodeType !== 3) {
+        return;
+      }
+      const text = node.nodeValue;
+      const words = text.split(" ");
+
+      words.forEach((word) => {
+        if (isTimestamp(word)) {
+          console.log(word);
+        }
+      });
+    });
+  });
 }
 
 loadContentScript();
