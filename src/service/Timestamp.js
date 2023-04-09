efa3b68abd923868196d88a37a56de385b420267
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-04-09 16:55:12 +0530

    feat: create func to see if a 13-digit timestamp is valid and falls between 2015 and 2030

---
+++ src/service/Timestamp.js
@@ -0,0 +1,17 @@
+const MIN_TIMESTAMP = 1420050600000;
+const MAX_TIMESTAMP = 1893436200000;
+
+export function isTimestamp(string) {
+  const timestamp = parseInt(string, 10);
+  if (Number.isNaN(timestamp)) {
+    return false;
+  }
+
+  if (!(MIN_TIMESTAMP <= timestamp && timestamp <= MAX_TIMESTAMP)) {
+    return false;
+  }
+
+  return true;
+}
+
+export function transformTimestamp() {}
