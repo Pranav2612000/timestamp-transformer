0db512553a972068684c1dc36645415a28809e22
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-04-19 07:35:54 +0530

    refactor: extract isTimestamp check to a seperate func

--- src/service/Timestamp.js
+++ src/service/Timestamp.js
@@ -1,13 +1,20 @@
 const MIN_TIMESTAMP = 1420050600000;
 const MAX_TIMESTAMP = 1893436200000;
 
+export function is13DigitTimestamp(timestamp) {
+  if (!(MIN_TIMESTAMP <= timestamp && timestamp <= MAX_TIMESTAMP)) {
+    return false;
+  }
+  return true;
+}
+
 export function isTimestamp(string) {
   const timestamp = parseInt(string, 10);
   if (Number.isNaN(timestamp)) {
     return false;
   }
 
-  if (!(MIN_TIMESTAMP <= timestamp && timestamp <= MAX_TIMESTAMP)) {
+  if (!is13DigitTimestamp(timestamp)) {
     return false;
   }
 
