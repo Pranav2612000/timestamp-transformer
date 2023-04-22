d280198bb75504fea6788a76ae7bd611870c90b6
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-04-22 20:54:43 +0530

    feat settings: create skeleton html for allowing user to choose start and end dates for auto transform

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,23 @@ import { React } from "react";
 
 function Scratchpad() {
   return (
-    <main className="scratchpad-container">
-      <h1> Hello World </h1>
-    </main>
+    <>
+      <h1> Settings </h1>
+      <main className="scratchpad-container">
+        <form>
+          <h3>Auto transform dates between:</h3>
+          <br />
+          <label htmlFor="start-date">
+            Start Date:
+            <input type="date" id="start-date" />
+          </label>
+          <label htmlFor="end-date">
+            End Date:
+            <input type="date" id="end-date" />
+          </label>
+        </form>
+      </main>
+    </>
   );
 }
 export default Scratchpad;
