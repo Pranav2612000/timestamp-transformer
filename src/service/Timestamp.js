4dc4c92f3f6b01ac3b4597e09924d6785cf1641f
Pranav2612000 <pranav2000joglekar@gmail.com>
2023-04-24 08:09:25 +0530

    feat: create func to display date according to a string format

--- src/service/Timestamp.js
+++ src/service/Timestamp.js
@@ -14,7 +14,23 @@ function _formatDate(timestamp, format) {
   if (!_validateDateFormat) {
     throw new Error("Unknown format");
   }
-  throw new Error("Format Date operation failed");
+  const date = new Date(timestamp);
+  const dateOfMonth = date.getDate();
+  const month = date.getMonth() + 1; // 0-indexed
+  const year = date.getFullYear();
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  let dateString = format;
+  dateString = dateString.replaceAll("DD", dateOfMonth);
+  dateString = dateString.replaceAll("MM", month);
+  dateString = dateString.replaceAll("YYYY", year);
+  dateString = dateString.replaceAll("hh", hour);
+  dateString = dateString.replaceAll("minutes", minutes);
+  dateString = dateString.replaceAll("seconds", seconds);
+
+  return dateString;
 }
 
 export function is13DigitTimestamp(timestamp, limits) {
